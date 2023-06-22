584c79c1c8826d4d3aa6f94858c807129f843b5e
leonardobalves <leonardobuckalves@gmail.com>
2023-06-21 22:56:46 -0300

    established one to many relationship

--- ApiCatalogo/Models/Category.cs
+++ ApiCatalogo/Models/Category.cs
@@ -5,5 +5,7 @@
         public int CategoryId { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
+
+        public ICollection<Product>? Products { get; set; }
     }
 }
--- ApiCatalogo/Models/Product.cs
+++ ApiCatalogo/Models/Product.cs
@@ -9,5 +9,8 @@
         public string? Image { get; set; }
         public DateTime BoughtDate { get; set; }
         public int Stock { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
     }
 }
