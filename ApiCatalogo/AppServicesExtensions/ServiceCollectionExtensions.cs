5272725f6cfb7c871de0a21951a3ea4a5eb55479
leonardobalves <leonardobuckalves@gmail.com>
2023-06-27 23:27:35 -0300

    Organizing code

---
+++ ApiCatalogo/AppServicesExtensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using ApiCatalog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalog.AppServicesExtensions
+{
+    public  static class ServiceCollectionExtensions
+    {
+        public static WebApplicationBuilder AddApiSwagger(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddSwagger();
+            return builder;
+        }
+
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            return services;
+        }
+
+        public static WebApplicationBuilder AddPersistence(this WebApplicationBuilder builder)
+        {
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+            return builder;
+        }
+    }
+}
--- ApiCatalogo/Program.cs
+++ ApiCatalogo/Program.cs
@@ -1,18 +1,11 @@
 using ApiCatalog.ApiEndpoints;
 using ApiCatalog.AppServicesExtensions;
-using ApiCatalog.Models;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+builder.AddApiSwagger();
+builder.AddPersistence();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
